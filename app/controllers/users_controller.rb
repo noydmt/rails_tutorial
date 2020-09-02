17aa833bd56aaf6b2327a34be7362879d3faac56
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-09-02 23:18:48 +0900

    create show action of Users Controller and show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  
+	def index
   end
+
+	def new
+	end
+
+	def create
+	end
+
+	def edit
+	end
+
+  # GET /users/:id
+	def show
+	  # user = User.first #local var => only show action
+		# @user = User.first #instance var => enable to use view(erb)
+		# @@user = User.first #global var => don't use as possible as you can
+
+		@user = User.find_by(id: params[:id])
+	end
+
+	def update
+	end
+
+	def destroy
+	end
+	
 end
