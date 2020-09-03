a80331cfa012d4af21caa06681f81e61871f23b4
NaoyaTsudome <noy.dmt1021@gmail.com>
2020-09-03 22:57:47 +0900

    create new,create action and error_messages partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
-  
+
 	def index
   end
 
+  # GET /users/new
 	def new
+	  @user = User.new
 	end
-
+  
+  # POST /users(+ params)
 	def create
+	  @user = User.new(user_params)
+		if @user.save
+		  redirect_to new_user_path
+		else
+      render 'new'
+		end
 	end
 
 	def edit
@@ -27,4 +36,12 @@ class UsersController < ApplicationController
 	def destroy
 	end
 	
+  private
+
+	def user_params
+	  # params のどの key を使用するのか限定する
+    # 悪意を持ったユーザーがparams[:user]の中の他のkeyをいじって送信しても受け取れないように。
+    params.require(:user).permit(:name,:email,:password,:password_confirmation)
+	end
+
 end
